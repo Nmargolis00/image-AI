61b9e7853754cabf329c7fafa0726695649bb915
John <johnwilday@yahoo.com>
2023-04-07 14:23:15 -0600

    Some styling of homepage

--- public/js/imagerender.js
+++
@@ -1,9 +0,0 @@
-const addPic = document.getElementById("get-photo")
-
-const image = localStorage.getItem("img")
-console.log(image)
-// let imageEl = document.createElement("img")
-// imageEl.setAttribute("src", image)
-
-// addPic.append(image)
-document.getElementById('image').src = image;
