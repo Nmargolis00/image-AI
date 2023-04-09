430b98d7009a687be87f12688af9cc5c9bb31fa7
John <johnwilday@yahoo.com>
2023-04-09 02:19:59 -0600

    Added signup feature and did some styling

--- public/js/imagesearch.js
+++ public/js/imagesearch.js
@@ -1,8 +1,23 @@
 const spinner = document.getElementById("spinner");
 const imageSearch = async () => {
   const prompt = document.querySelector("#term").value.trim();
-
+const hideImage = document.getElementById("image")
   const size = document.querySelector("#pic-size").value.trim();
+  const dynamic = document.querySelector("#dynamic-box");
+
+   if (size == "1024x1024") {
+        dynamic.setAttribute("style", "margin-left: 5%;");
+        document.querySelector("#image").style.display = "none";
+        
+      }
+      else if (size == "512x512") {
+          dynamic.setAttribute("style", "margin-left: 13%;");
+          document.querySelector("#image").style.display = "none";
+      }
+      else {
+        dynamic.setAttribute("style", "margin-left: 22%;");
+        document.querySelector("#image").style.display = "none";
+      }
 
   if (!prompt || !size) {
     alert("size and text required");
@@ -27,12 +42,16 @@ async function generateImageRequest(prompt, size) {
       throw new Error("That image could not be generated");
     } else {
       const imageUrl = await response.json();
-      document.querySelector("#image").src = imageUrl.photo;
+      
+      document.querySelector("#image").src = await imageUrl.photo;
+      document.querySelector("#image").style.display = "block";
       document.getElementById("spinner").style.display = "none";
     }
+   
   } catch (error) {
     console.log(console.log(error));
   }
+  
 }
 window.addEventListener("load", () => {
   document.getElementById("spinner").style.display = "none";
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -18,26 +18,30 @@ router.get("/", async (req, res) => {
   const mapPhotos = getPhotos.map((one) => one.get({ plain: true }));
   const orderPhotos = mapPhotos.reverse()
   
-  res.render("community", {pic: orderPhotos});
+  res.render("community", {pic: orderPhotos, logged_in: req.session.logged_in});
 }
 else {
   res.render("community")
 }
 });
 router.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", {logged_in: req.session.logged_in});
 });
 
 router.get("/images", (req, res) => {
   res.render("homepage", { logged_in: req.session.logged_in });
 });
 
+router.get("/signup", (req, res) => {
+  res.render("signup", { logged_in: req.session.logged_in });
+});
+
 router.get("/home", (req, res) => {
-  res.render("homepage");
+  res.render("homepage", { logged_in: req.session.logged_in });
 });
 
 router.get("/show-image", (req, res) => {
-  res.render("show-image", { photo: req.session.photo });
+  res.render("show-image", { photo: req.session.photo, logged_in: req.session.logged_in });
 });
 
 //router.get('/signup', (req, res) => {
